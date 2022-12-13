e067be860b2c8174d276d4a02db2e34c08c0c6be
Minoki <fongjunyi11@gmail.com>
2022-12-13 18:26:17 +0800

    update - JunYi

--- administration/category.php
+++ administration/category.php
@@ -27,8 +27,36 @@ require 'static-nav/static-sidenav.php';
                     Category Table
 
                     <button type="button" class="btn btn-primary m-md-2 float-end" data-bs-toggle="modal" data-bs-target="#categoryModal">Create</button>
-                    <?php include"crud_category/category_create.php"; ?>
-
+                    <div class="modal fade" id="categoryModal" tabindex="-1" role="dialog" aria-labelledby="categoryModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="categoryModalLabel">Category Create</h5>
+                                    <span aria-hidden="true" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-x-lg"></i></span>
+                                </div>
+                                <div>
+                                    <i class="bi bi-question-circle float-end" data-bs-toggle="popover" title="Description:" data-bs-content="<?php echo $f_Desc1; ?>"></i>
+                                    <span style="color: #dc3545">&nbsp;&nbsp; * = Required</span>
+                                </div>
+                                <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                                    <div class="modal-body">
+                                        <div class="form-group">
+                                            <label>Category Name</label><span style="color: #dc3545">&nbsp;&nbsp; *<?php echo $nameErr; ?></span>
+                                            <input type="text" name="name" class="form-control" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Description</label><span style="color: #dc3545">&nbsp;&nbsp; *<?php echo $descriptionErr; ?></span>
+                                            <textarea name="description" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary" name="create_category">Confirm</button>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-body">
                     <table class="table">
@@ -71,6 +99,13 @@ require 'static-nav/static-sidenav.php';
 <?php include 'static-include/footer.php'; ?>
 
 <script>
+
+    //popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (t) {
+        return new bootstrap.Popover(t);
+    });
+
     //delete category confirmation
     $(document).on('click', '.delete_category', function () {
         var id = $(this).attr('id');
