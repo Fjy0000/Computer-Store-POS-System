3fdd731c0c254ae7f047d0dabc1253435da302b0
Minoki <fongjunyi11@gmail.com>
2022-12-14 07:27:20 +0800

    update - JunYi

--- administration/resetPassword.php
+++ administration/resetPassword.php
@@ -41,6 +41,7 @@ if (isset($_POST['resetPassword'])) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <title>Reset Password</title>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
--- administration/setNewPassword.php
+++ administration/setNewPassword.php
@@ -56,6 +56,7 @@ if (isset($_POST['confirmResetPassword'])) {
 
 <html lang="en">
     <head>
+        <title>set new password</title>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
