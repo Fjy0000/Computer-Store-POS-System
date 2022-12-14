09fa12596e3a684e92b5cff2e21793b5c28814bd
Minoki <fongjunyi11@gmail.com>
2022-12-15 05:40:39 +0800

    update - JunYi

--- administration/liveChat.php
+++ administration/liveChat.php
@@ -18,8 +18,8 @@ session_start();
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <!-- Jquery only -->
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <link href="css/livechat_box_styles.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -66,7 +66,6 @@ session_start();
                 }
             })
         }
-
         function update_activity_status() {
             $.ajax({
                 url: "liveChat_function/update_activity_status.php",
@@ -74,7 +73,6 @@ session_start();
                 }
             })
         }
-
         function chat_box(to_user_id, to_user_name) {
             var modal_content = '<div id="user_dialog_' + to_user_id + '" class="user_dialog" title="You have chat with ' + to_user_name + ' ">';
             modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="' + to_user_id + '" id="chat_history_' + to_user_id + '">';
@@ -96,7 +94,6 @@ session_start();
             });
             $('#user_dialog_' + to_user_id).dialog('open');
         });
-
         $(document).on('click', '.send_chat', function () {
             var to_user_id = $(this).attr('id');
             var chat_message = $('#chat_message_' + to_user_id).val();
@@ -111,7 +108,6 @@ session_start();
                 }
             })
         });
-
         function get_to_user_chat_history(to_user_id) {
             $.ajax({
                 url: "liveChat_function/get_to_user_chat_history.php",
@@ -124,18 +120,15 @@ session_start();
                 }
             })
         }
-
         function update_chat_history() {
             $('.chat_history').each(function () {
                 var to_user_id = $(this).data('touserid');
                 get_to_user_chat_history(to_user_id);
             });
         }
-
         $(document).on('click', '.ui-button-icon', function () {
             $('.user_dialog').dialog('destroy').remove();
         });
-
         $(document).on('click', '.remove_message', function () {
             var message_id = $(this).attr('id');
             if (confirm("Are you sure you want to remove this message in the chat ?")) {
@@ -149,6 +142,5 @@ session_start();
                 })
             }
         });
-
     });
 </script>
