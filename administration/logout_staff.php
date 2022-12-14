11d16b410cdb91f09181a61f25937fe642f5eb24
Minoki <fongjunyi11@gmail.com>
2022-12-15 05:43:07 +0800

    update - JunYi

--- administration/logout_staff.php
+++ administration/logout_staff.php
@@ -1,16 +1,12 @@
 <?php
-
 require ($_SERVER['DOCUMENT_ROOT'] . '/Computer-Store-POS-System/administration/dbconnect.php');
-
-//logout clear session value
+//logout remove user login status
 session_start();
-
 $id = $_SESSION['id'];
 $sql = "DELETE FROM login_details WHERE user_id='$id'";
 $sql_run = mysqli_query($connect, $sql);
-
+//logout clear session value
 session_destroy();
-
 header("Location:http://localhost/Computer-Store-POS-System/administration/login_staff.php");
 exit(0);
 
